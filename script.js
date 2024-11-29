098ce557cd424ccafe8802531eb4c5223ed66591
Caselles17 <johnathan.caselles@gmail.com>
2024-11-29 17:47:03 +0100

    Pagina lista
    Falta hacer los header flotantes

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.querySelector('.hamburger-icon').addEventListener('click', function() {
+  // Toggle la clase 'open' en el icono y el menú
+  this.classList.toggle('open');
+  document.querySelector('.menu-links').classList.toggle('open');
+});
