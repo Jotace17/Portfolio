ed9c33e4e02c42006b9173a70f3868eb1e6da1f9
Caselles17 <johnathan.caselles@gmail.com>
2024-11-29 20:11:48 +0100

    finally?

--- script.js
+++ script.js
@@ -4,9 +4,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-document.querySelector('.hamburger-icon').addEventListener('click', function() {
-  // Toggle la clase 'open' en el icono y el menú
-  this.classList.toggle('open');
-  document.querySelector('.menu-links').classList.toggle('open');
-});
