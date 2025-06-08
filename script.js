af361db1e752c15e0c9d9e5115dbf54eacd7b47d
Caselles17 <johnathan.caselles@gmail.com>
2025-06-08 23:07:31 +0200

    fixed project page

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let currentIndex = 0;
+function moveCarousel(direction) {
+  const track = document.getElementById('carousel-track');
+  const images = track.querySelectorAll('img');
+  const total = images.length;
+
+  currentIndex = (currentIndex + direction + total) % total;
+  track.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
